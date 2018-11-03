753ca86f85c7b1ac9a3b4a8c9c6ef17ad41d3e8c
walker <walket1239@gmail.com>
2018-11-03 17:35:18 -0500

    Update strategy/main.cpp

--- strategy/main.cpp
+++ strategy/main.cpp
@@ -1,11 +1,24 @@
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
+#include <iostream>
+#include "CImg.h"
+using namespace std;
+using namespace cimg_library;
+int main()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+  CImg<int> img("walker.bmp");
+  cout<<endl<<endl<<endl;
+  int *r=img.data(0,0,0,0);
+  int *g=img.data(0,0,0,1);
+  int *b=img.data(0,0,0,2);
+  
+  int *x=new int[img.width()*img.height()];
+  for (int i = 0; i < img.width()*img.height(); ++i)
+  {
+    x[i]=(r[i]+g[i]+b[i])/3;
+  }
+  CImg<int> gris(x,img.width(),img.height(),1,1,1);
+  /*for (int i = 0; i < 100; ++i)
+    cout<<img.data()[i];
+  cout<<endl<<endl<<endl;
+  */(img,gris).display();
+  return 0;
 }
